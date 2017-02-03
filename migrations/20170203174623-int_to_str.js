e370e6b0940959541552252e206e242db233a849
weirui222 <congcongli@live.com>
2017-02-03 09:49:44 -0800

    adding migration to convert bus from int to str

---
+++ migrations/20170203174623-int_to_str.js
@@ -0,0 +1,39 @@
+'use strict';
+
+module.exports = {
+  up: function (queryInterface, Sequelize) {
+    /*
+      Add altering commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.createTable('users', { id: Sequelize.INTEGER });
+    */
+    return queryInterface.changeColumn(
+  		'favorites',
+  		'bus',
+		  {
+		    type: Sequelize.STRING,
+		    allowNull: false
+		  }
+		)
+  },
+
+  down: function (queryInterface, Sequelize) {
+    /*
+      Add reverting commands here.
+      Return a promise to correctly handle asynchronicity.
+
+      Example:
+      return queryInterface.dropTable('users');
+    */
+    return queryInterface.changeColumn(
+  		'favorites',
+  		'bus',
+		  {
+		    type: Sequelize.INTEGER,
+		    allowNull: false
+		  }
+		)
+  }
+};
