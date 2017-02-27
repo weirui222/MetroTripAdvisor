9bb227016fa05c4ea38af17b39c10d1c81bd3979
weirui222 <congcongli@live.com>
2017-02-27 14:00:04 -0800

    delete signin/up

--- client/src/Navbar.js
+++ client/src/Navbar.js
@@ -7,17 +7,12 @@ class Navbar extends Component {
      <nav className="navbar navbar-inverse navbar-static-top">
        <div className="container-fluid">
          <div className="navbar-header">
-           <a className="navbar-brand" href={`/`}>Metro Trip Advisor</a>
+           <a className="navbar-brand" id='brand' href={`/`}>Metro Trip Advisor</a>
          </div>
          <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-           <ul className="nav navbar-nav">
-             <li><a href={`/login`}>Login or Signup <span className="sr-only">(current)</span></a></li>
-             <li><a href="#">Recent</a></li>
-           </ul>
            <ul className="nav navbar-nav navbar-right">
-             <img src={icon}/>
+             <img className="navbarimg" src={icon}/>
            </ul>
-           <p>{this.props.username} </p>
          </div>
        </div>
      </nav>
